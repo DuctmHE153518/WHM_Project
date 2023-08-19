cd758e8db3392c933b0d4c391f6a5c7c2a22d77d
DuctmHE153518 <97203297+DuctmHE153518@users.noreply.github.com>
2023-08-19 18:52:21 +0700

    tao them form window cho client

---
+++ WHM_Client/Client_Project13/ClientWHM/QLXuatKhoWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClientWHM
+{
+    /// <summary>
+    /// Interaction logic for QLXuatKhoWindow.xaml
+    /// </summary>
+    public partial class QLXuatKhoWindow : Window
+    {
+        public QLXuatKhoWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
