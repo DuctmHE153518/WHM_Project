69982c2c2a6bfbf62f47d3523cacc5d0101faa19
DuctmHE153518 <97203297+DuctmHE153518@users.noreply.github.com>
2023-08-21 21:12:58 +0700

    sua form client và update dbcontext

--- WHM_Api/Api_Project13/ApiWHM/Models/WhmanagementContext.cs
+++ WHM_Api/Api_Project13/ApiWHM/Models/WhmanagementContext.cs
@@ -34,9 +34,14 @@ public partial class WhmanagementContext : DbContext
     public virtual DbSet<Xuatkho> Xuatkhos { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server =DUCTRAN\\SQLEXPRESS; database = WHManagement; uid=sa; pwd=2001; Integrated Security=True;TrustServerCertificate=True");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("WHMConStr"));
+        }
 
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Chitiethoadon>(entity =>
